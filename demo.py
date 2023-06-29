7a2f373b02b0c5fdd98f6ff7f5041c2ae542de76
罗凯 <lk@addcn.com>
2023-06-29 12:46:55 +0800

    update

--- demo.py
+++ demo.py
@@ -5,30 +5,52 @@ import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
 
-
+# Import the backtrader platform
 import backtrader as bt
-import backtrader.indicators as btind
-import backtrader.feeds as btfeeds
 
-class MyStrategy(bt.Strategy):
-    params = dict(period=20)
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        self.bt_sma = bt.indicators.MovingAverageSimple(self.dataclose)
+        self.buy_sell_signal = bt.indicators.CrossOver(self.dataclose, self.bt_sma)
 
-        self.movav = btind.SimpleMovingAverage(self.data, period=self.p.period)
+    def start(self):
+        print("the start call me!")
+
+    def prenext(self):
+        print("prenext_open call me!")
+
+    def nextstart(self):
+        print("nextstart call me!")
 
     def next(self):
-        print(f'self.movav.lines.sma[0]:{self.movav.lines.sma[0]} , self.data.lines.close[0]:{self.data.lines.close[0]}')
-        if self.movav.lines.sma[0] > self.data.lines.close[0]:
-            print('Simple Moving Average is greater than the closing price')
+        print("next call me!")
+
+        if not self.position and self.buy_sell_signal[0] == 1:
+            self.order=self.close()
+            self.order = self.buy()
+
+        if not self.position and self.buy_sell_signal[0] == -1:
+            self.order = self.close()
+            self.order = self.sell()
 
 
 if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
 
-    # Datas are in a subfolder of the samples. Need to find where the script is
-    # because it could have been called from anywhere
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     datapath = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
 
@@ -36,9 +58,10 @@ if __name__ == '__main__':
     data = bt.feeds.YahooFinanceCSVData(
         dataname=datapath,
         # Do not pass values before this date
-        fromdate=datetime.datetime(2000, 1, 1),
-        # Do not pass values after this date
+        fromdate=datetime.datetime(2000, 5, 1),
+        # Do not pass values before this date
         todate=datetime.datetime(2000, 12, 31),
+        # Do not pass values after this date
         reverse=False)
 
     # Add the Data Feed to Cerebro
@@ -55,3 +78,5 @@ if __name__ == '__main__':
 
     # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot(style='candle')
