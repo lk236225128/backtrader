9ee5fe86b7812a4e49c08579fa917eb19f330a75
罗凯 <lk@addcn.com>
2023-07-07 18:50:37 +0800

    beta2

--- jp22.py
+++ jp22.py
@@ -42,7 +42,7 @@ class TestStrategy(bt.Strategy):
         self.buycomm = None
 
         # Add a MovingAverageSimple indicator
-        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.maperiod)
+        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.maperiod, plot=False)
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -92,47 +92,27 @@ class TestStrategy(bt.Strategy):
 
         # Check if we are in the market
         if not self.position:
-            if self.data0.lines.JJ9[0] >= 4:
-                print(f'JJ9 :{self.data0.lines.JJ9[0]}')
-                # print(f'self.datas:{self.datas[0].JJ9}')
-                # BUY, BUY, BUY!!!
-                self.log('BUY CREATE, %.2f' % self.dataclose[0], doprint=True)
+            if self.data0.lines.JJ9[0] >= 4 \
+                    or self.data0.lines.JJ6[0] >= 4 \
+                    or (self.data0.lines.ZJDC[-1] >= 100 and self.data0.lines.ZJDC[0] < 100):
+                self.log(
+                    f'BUY CREATE: {self.dataclose[0]} , '
+                    f'JJ9: {self.data0.lines.JJ9[0]} , '
+                    f'JJ6: {self.data0.lines.JJ6[0]} , '
+                    f'ZJDC : {self.data0.lines.ZJDC[-1]} -> {self.data0.lines.ZJDC[0]}',
+                    doprint=True)
 
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy()
-
+                self.order = self.buy()  # Keep track of the created order to avoid a 2nd order
         else:
             # Already in the market ... we might sell
             if len(self) >= (self.bar_executed + 5):
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
 
 
-class PandasData_more(bt.feeds.PandasData):
-    lines = ('JJ9',)  # 要添加的线
-    # 设置 line 在数据源上的列位置
-    params = (
-        ('JJ9', -1),  # -1表示自动按列明匹配数据，也可以设置为线在数据源中列的位置索引 (('pe',6),('pb',7),)
-    )
-
-
-if __name__ == '__main__':
-    # 设置显示选项以显示完整的数据内容
-    pd.set_option('display.max_rows', None)  # 显示所有行
-    pd.set_option('display.max_columns', None)  # 显示所有列
-    pd.set_option('display.width', None)  # 自动调整输出宽度
-
-    # Create a cerebro entity
-    cerebro = bt.Cerebro()
-
-    # Add a strategy
-    cerebro.addstrategy(TestStrategy)
-
-
-    def add_JJ9(df):
+class MyIndicators:
+    def add_JJ9(self, df):
         # 新增字段
         VAR1 = (df['high'] + df['low'] + df['open'] + 2 * df['close']) / 5
         VAR2 = REF(VAR1, 1)
@@ -148,34 +128,100 @@ if __name__ == '__main__':
                     condition3.astype('int64') + \
                     condition4.astype('int64')
         # df = df[['open', 'high', 'low', 'close', 'volume', 'openinterest', 'JJ9']]
+        return df
+
+    def add_JJ6(self, df):
+        VAR1 = df['volume'] >= 1.3 * MA(df['volume'], 5)
+
+        VAR4 = (df['close'] - REF(df['open'], 29)) / REF(df['open'], 29) * 100
+
+        VAR5 = EMA(0.667 * REF(VAR4, 1) + 0.333 * VAR4, 5)
+
+        condition1 = VAR1.astype('bool')
+
+        condition2 = COUNT(VAR4 < -17, 3).astype('bool')
+
+        condition3 = COUNT(VAR4 >= VAR5, 3).astype('bool')
+
+        condition4 = (REF(df['low'], 1) == LLV(df['low'], 120))
+
+        df["JJ6"] = condition1.astype('int64') + \
+                    condition2.astype('int64') + \
+                    condition3.astype('int64') + \
+                    condition4.astype('int64')
+        return df
+
+    def add_ZJDC(self, df):
+        VAR2 = REF(df['low'], 1)
+
+        VAR3 = SMA(ABS(df['low'] - VAR2), 3, 1) / SMA(MAX(df['low'] - VAR2, 0), 3, 1) * 100
+
+        VAR4 = EMA(IF(df['close'] * 1.3, VAR3 * 10, VAR3 / 10), 3)
+
+        VAR5 = LLV(df['low'], 30)
+
+        VAR6 = HHV(VAR4, 30)
+
+        VAR7 = IF(MA(df['close'], 58), 1, 0)
+
+        VAR8 = EMA(IF(df['low'] <= VAR5, (VAR4 + VAR6 * 2) / 2, 0), 3) / 618 * VAR7
+
+        df["ZJDC"] = RD(VAR8, 2)
+
+        return df
+
+
+class PandasData_more(bt.feeds.PandasData):
+    lines = ('JJ9', 'JJ6', 'ZJDC')  # 要添加的线
+    # 设置 line 在数据源上的列位置
+    params = (
+        ('JJ6', -1),
+        ('JJ9', -1),  # -1表示自动按列明匹配数据，也可以设置为线在数据源中列的位置索引 (('pe',6),('pb',7),)
+        ('ZJDC', -1),
+    )
+
+
+if __name__ == '__main__':
+    # 设置显示选项以显示完整的数据内容
+    pd.set_option('display.max_rows', None)  # 显示所有行
+    pd.set_option('display.max_columns', None)  # 显示所有列
+    pd.set_option('display.width', None)  # 自动调整输出宽度
+
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    mi = MyIndicators()
+
+
+    # Create a Data Feed, 使用tushare旧版接口获取数据
+    def get_data(code, start='2022-11-01', end='2023-07-07'):
+        df = ts.get_hist_data(code, ktype='D', start=start, end=end)
+        df['openinterest'] = 0
+        df = df.sort_index()  # 按索引排序
         df.index = pd.to_datetime(df.index)  # 将索引转换为datetime类型
+        df = mi.add_JJ9(df)
+        df = mi.add_JJ6(df)
+        df = mi.add_ZJDC(df)
         print(f'df:{df}')
         return df
 
 
-    # Create a Data Feed, 使用tushare旧版接口获取数据
-    def get_data(code, start='2023-03-31', end='2023-06-24'):
-        df = ts.get_hist_data(code, ktype='D', start=start, end=end)
-        df['openinterest'] = 0
-        df = df.sort_index()  # 按索引排序
-        df = add_JJ9(df)
-        return df
+    dataframe = get_data('601012')  # 合盛硅业
 
-
-    dataframe = get_data('000568')  # 合盛硅业
+    # 新定义的PandasData_more（继承自bt.feeds.PandasData）
+    data = PandasData_more(dataname=dataframe)
+    cerebro.adddata(data)
 
     # 加载数据
     # data = bt.feeds.PandasData(dataname=dataframe)
-
-    # 这里使用上述定义的新类PandasData_more（继承了bt.feeds.PandasData）
-    datafeed1 = PandasData_more(dataname=dataframe)
-    cerebro.adddata(datafeed1)
-
     # Add the Data Feed to Cerebro
     # cerebro.adddata(data)
 
     # Set our desired cash start
-    cerebro.broker.setcash(1000.0)
+    cerebro.broker.setcash(100000.0)
 
     # Add a FixedSize sizer according to the stake
     cerebro.addsizer(bt.sizers.FixedSize, stake=10)
@@ -185,3 +231,6 @@ if __name__ == '__main__':
 
     # Run over everything
     cerebro.run(maxcpus=1)
+
+    # Plot the result
+    # cerebro.plot(style='candle')
